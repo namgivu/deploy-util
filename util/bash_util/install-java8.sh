c8d0cd995c78000c3ef0e9cef45590371bea317a
namgivu <namgivu@gmail.com>
2017-10-13 11:22:21 +0700

    install java 8 util - config java

--- util/bash_util/install-java8.sh
+++ util/bash_util/install-java8.sh
@@ -4,5 +4,12 @@
 sudo apt update
 sudo apt-get install -y default-jdk
 
-#config default java (optional) ref.https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-get-on-ubuntu-16-04#managing-java
-:: sudo update-alternatives --config java
+#region config java (optional)
+
+  #config default java version ref. https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-get-on-ubuntu-16-04#managing-java
+  :: sudo update-alternatives --config java
+
+  #config java e.g. JAVA_HOME ref. https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-get-on-ubuntu-16-04#setting-the-java_home-environment-variable
+  :: sudo nano /etc/environment #set JAVA_HOME="/usr/PATH/TO/YOUR/JDK"
+
+#endregion
