397ddc7fcce2ce6236b844f52ad704db29fedc42
namgivu <namgivu@gmail.com>
2017-10-13 11:18:27 +0700

    install java 8 util - default config

--- util/bash_util/install-java8.sh
+++ util/bash_util/install-java8.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+#install java 8
 sudo apt update
 sudo apt-get install -y default-jdk
+
+#config default java (optional) ref.https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-get-on-ubuntu-16-04#managing-java
+:: sudo update-alternatives --config java
