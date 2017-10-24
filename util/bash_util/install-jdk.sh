c06e4e6c1ea605eb8a8bb7867cb1772bb83a9088
namgivu <namgivu@gmail.com>
2017-10-24 11:15:05 +0700

    review jdk-related util

--- util/bash_util/install-jdk.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-: #ref. https://stackoverflow.com/a/14788468/248616
-: #ref. https://askubuntu.com/a/755854/22308
-
-sudo apt update
-sudo apt-get install -y openjdk-8-jdk
-sudo apt-get install -y openjdk-8-source #this is optional, the jdk source code
