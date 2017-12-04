32c6d62c6b9cd594c07e2b089ef5c2336db09355
namgivu <namgivu@gmail.com>
2017-12-04 16:03:35 +0700

    update java install

--- util/bash_util/install-java-8.sh
+++ util/bash_util/install-java-8.sh
@@ -13,11 +13,14 @@ sudo apt install -y openjdk-8-source #this is optional, the jdk source code - yo
   #config default java version ref. https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-get-on-ubuntu-16-04#managing-java
   : sudo update-alternatives --config java
 
-  #config java e.g. JAVA_HOME ref. https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-get-on-ubuntu-16-04#setting-the-java_home-environment-variable
+  #config JAVA_HOME ref. https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-get-on-ubuntu-16-04#setting-the-java_home-environment-variable
   ls -la `which java`           #get /usr/bin/java -> /etc/alternatives/java
   ls -la /etc/alternatives/java #get /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java -> JAVA_HOME='/usr/lib/jvm/java-8-openjdk-amd64'
   : sudo nano /etc/environment  #set JAVA_HOME="/usr/PATH/TO/YOUR/JDK" e.g. JAVA_HOME='/usr/lib/jvm/java-8-openjdk-amd64'
   : nano $HOME/.bashrc #add line `source /etc/environment` ref. https://askubuntu.com/a/175547/22308
+
+  : #config SDK for IntelliJ (if you use it) ref. https://trello.com/c/S74p81io/480-jdk-source-in-intellij
+
 
 #endregion
 
