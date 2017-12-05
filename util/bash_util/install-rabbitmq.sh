07c222451df715625108afbc1296f8673178828c
namgivu <namgivu@gmail.com>
2017-12-05 21:32:55 +0700

    install rabbitmq via docker run instead

--- util/bash_util/install-rabbitmq.sh
+++ util/bash_util/install-rabbitmq.sh
@@ -1,40 +1,18 @@
 #!/usr/bin/env bash
 
-: #ref. https://www.rabbitmq.com/install-debian.html
+: #we don't install but use docker instead
+: #ref. https://hub.docker.com/_/rabbitmq/
 
-#install prerequisite erlang/otp ref. https://stackoverflow.com/a/44690857/248616
-cd "$HOME/Downloads" && \
-  wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && \
-  sudo dpkg -i erlang-solutions_1.0_all.deb && \
-  sudo apt update && sudo apt install -y esl-erlang=1:19.3.6 && \
-cd --
+#start
+DOCKER_IMAGE='rabbitmq:3-management'
+RABBITMQ_NAME='my-rabbitmq'                   #need name for each running instance to persist data
+RABBITMQ_USER='guest' ; RABBITMQ_PASS='guest' #login localhost:15672 with this user+password
+docker run -d \
+  --hostname ${RABBITMQ_NAME} \
+  --name ${RABBITMQ_NAME} \
+  -e RABBITMQ_DEFAULT_USER=${RABBITMQ_USER} \
+  -e RABBITMQ_DEFAULT_PASS=${RABBITMQ_PASS} \
+  -p 15672:15672 ${DOCKER_IMAGE}
 
-
-#add the Apt repository to your Apt source list
-echo "deb https://dl.bintray.com/rabbitmq/debian xenial main" | sudo tee /etc/apt/sources.list.d/bintray.rabbitmq.list
-
-#add rabbitmq public key to trusted key list
-wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
-
-#do install rabbitmq
-sudo apt update && sudo apt install -y rabbitmq-server
-
-#pinning erlang version #TODO consider using apt hold ref. https://askubuntu.com/a/18656/22308
-sudo sh -c "
-echo '
-  Package: erlang*
-  Pin: version 1:19.3-1
-  Pin-Priority: 1000
-
-  Package: esl-erlang
-  Pin: version 1:19.3.6
-  Pin-Priority: 1000
-' >> /etc/apt/preferences" #pin file ref. https://help.ubuntu.com/community/PinningHowto
-sudo apt-cache policy #check pinned package #TODO why this is not working i.e. 'Pinned packages:' line is empty
-
-#aftermath check
-sudo rabbitmqctl status
-
-#stop/start reference
-: sudo rabbitmqctl stop
-: service rabbitmq-server start
+#stop
+n=${RABBITMQ_NAME} && docker stop $n && docker rm $n
