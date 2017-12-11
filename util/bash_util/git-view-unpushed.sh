2af7491ddd4199106a0d7d4f8ca07555da0c3e87
namgivu <namgivu@gmail.com>
2017-12-11 18:16:22 +0700

    git unpushed - WIP

--- util/bash_util/git-view-unpushed.sh
+++ util/bash_util/git-view-unpushed.sh
@@ -14,18 +14,25 @@ REPOS=`find "$FROM" -name '.git' `
 
 #check repo 1-by-1
 FOUND_LOCAL_BRANCHES='' #storing result for local branches that not pushed
+FOUND_LOCAL_COMMITS='' #storing result for local commits that not pushed
 function checkUnpushed() {
   REPO=$1
-  cd "$REPO/.." #go to repo folder
+  s="$REPO/.." ; s=$(cd "$s" && pwd) ; REPO_FOLDER="$s"
+  cd "$REPO_FOLDER" #go to repo folder
 
   #view unpushed branch of local repo ref. https://stackoverflow.com/a/15671218/248616
   localBranches=`git branch | sed s,..,,`
   for localBranch in ${localBranches}; do
     git config --get "branch.$localBranch.remote" | sed Q1 && \
       echo "$localBranch" && \
-
       FOUND_LOCAL_BRANCHES="$FOUND_LOCAL_BRANCHES\n branch=$localBranch;repo=$REPO" #record found unpushed local branch
   done
+
+  #view unpushed commit of local repo ref. https://stackoverflow.com/a/15671218/248616
+  commits=`git log origin/master..HEAD` #TODO replace origin/master by current checkout branch #TODO this command NOT WORKING to get unpushed commits
+  if [ ! -z "$commits" ]; then
+    FOUND_LOCAL_COMMITS="$FOUND_LOCAL_COMMITS\n$REPO_FOLDER"
+  fi
 
   cd -- #return to where we were
 }
@@ -36,8 +43,15 @@ for REPO in ${REPOS}; do
 done
 
 #output printing
-FOUND_LOCAL_BRANCHES=`printf "$FOUND_LOCAL_BRANCHES"` #split string by new-line
-echo && echo "Found local branches" #TODO how to print length of $FOUND_LOCAL_BRANCHES; this not working #echo && echo "Found local branches (${#FOUND_LOCAL_BRANCHES[@]} found)"
-for f in ${FOUND_LOCAL_BRANCHES}; do
-  echo "$f"
-done
+  FOUND_LOCAL_BRANCHES=`printf "$FOUND_LOCAL_BRANCHES"` #split string by new-line
+  FOUND_LOCAL_COMMITS=`printf "$FOUND_LOCAL_COMMITS"` #split string by new-line
+
+  echo && echo "Found local branches" #TODO how to print length of $FOUND_LOCAL_BRANCHES; this not working #echo && echo "Found local branches (${#FOUND_LOCAL_BRANCHES[@]} found)"
+  for f in ${FOUND_LOCAL_BRANCHES}; do
+    echo "$f"
+  done
+
+  echo && echo "Found local commits" #TODO how to print length of $FOUND_LOCAL_COMMITS; as above
+  for f in ${FOUND_LOCAL_COMMITS}; do
+    echo "cd $f; git push; cd --"
+  done
