7d1db1cbf4872784ee32e89fa8a058af031f373c
namgivu <namgivu@gmail.com>
2017-12-12 11:57:09 +0700

    git unpushed - DONE

--- util/bash_util/git-view-unpushed.sh
+++ util/bash_util/git-view-unpushed.sh
@@ -13,8 +13,8 @@ fi
 REPOS=`find "$FROM" -name '.git' `
 
 #check repo 1-by-1
-FOUND_LOCAL_BRANCHES='' #storing result for local branches that not pushed
-FOUND_LOCAL_COMMITS='' #storing result for local commits that not pushed
+UNPUSHED_BRANCHES='' #storing result for local branches that not pushed
+UNPUSHED_COMMITS='' #storing result for local commits that not pushed
 function checkUnpushed() {
   REPO=$1
   s="$REPO/.." ; s=$(cd "$s" && pwd) ; REPO_FOLDER="$s"
@@ -25,33 +25,38 @@ function checkUnpushed() {
   for localBranch in ${localBranches}; do
     git config --get "branch.$localBranch.remote" | sed Q1 && \
       echo "$localBranch" && \
-      FOUND_LOCAL_BRANCHES="$FOUND_LOCAL_BRANCHES\n branch=$localBranch;repo=$REPO" #record found unpushed local branch
+      UNPUSHED_BRANCHES="$UNPUSHED_BRANCHES\n branch=$localBranch;repo=$REPO" #record found unpushed local branch
   done
 
   #view unpushed commit of local repo ref. https://stackoverflow.com/a/15671218/248616
-  commits=`git log origin/master..HEAD` #TODO replace origin/master by current checkout branch #TODO this command NOT WORKING to get unpushed commits
-  if [ ! -z "$commits" ]; then
-    FOUND_LOCAL_COMMITS="$FOUND_LOCAL_COMMITS\n$REPO_FOLDER"
-  fi
+  for localBranch in ${localBranches}; do
+    git rev-parse --verify "$localBranch" > /dev/null ; errCode=$? #check if branch exists ref.https://stackoverflow.com/q/5167957/248616
+    if [ "$errCode" -eq "0" ]; then #branh does exist
+      commits=`git log origin/${localBranch}..${localBranch}` #view unpushed commit of local repo ref. https://stackoverflow.com/a/15671218/248616
+      if [ ! -z "$commits" ]; then
+        UNPUSHED_COMMITS="$UNPUSHED_COMMITS\n repo=$REPO_FOLDER;branch=$localBranch"
+      fi
+    fi
+  done
 
   cd -- #return to where we were
 }
-
 for REPO in ${REPOS}; do
   echo "Checking $REPO"
   checkUnpushed "$REPO"
 done
 
 #output printing
-  FOUND_LOCAL_BRANCHES=`printf "$FOUND_LOCAL_BRANCHES"` #split string by new-line
-  FOUND_LOCAL_COMMITS=`printf "$FOUND_LOCAL_COMMITS"` #split string by new-line
+UNPUSHED_BRANCHES=`printf "$UNPUSHED_BRANCHES"` #split string by new-line
+echo && echo "Found local branches" #TODO how to print length of $UNPUSHED_BRANCHES; this not working #echo && echo "Found local branches (${#UNPUSHED_BRANCHES[@]} found)"
+for f in ${UNPUSHED_BRANCHES}; do
+  echo "$f"
+done
 
-  echo && echo "Found local branches" #TODO how to print length of $FOUND_LOCAL_BRANCHES; this not working #echo && echo "Found local branches (${#FOUND_LOCAL_BRANCHES[@]} found)"
-  for f in ${FOUND_LOCAL_BRANCHES}; do
-    echo "$f"
-  done
+UNPUSHED_COMMITS=`printf "$UNPUSHED_COMMITS"` #split string by new-line
+echo && echo "Found local commits" #TODO how to print length of $UNPUSHED_COMMITS; as above
+for f in ${UNPUSHED_COMMITS}; do
+  echo "$f"
+done
 
-  echo && echo "Found local commits" #TODO how to print length of $FOUND_LOCAL_COMMITS; as above
-  for f in ${FOUND_LOCAL_COMMITS}; do
-    echo "cd $f; git push; cd --"
-  done
+#TODO also take care of uncommitted local changes
