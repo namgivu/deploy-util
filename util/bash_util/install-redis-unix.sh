e2669c514f4fe4b814b810b97ae8a236d6a2cf03
namgivu <namgivu@gmail.com>
2018-02-16 10:48:43 +0700

    install redis util

--- util/bash_util/install-redis-unix.sh
+++ util/bash_util/install-redis-unix.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #install redis on Mac OS
-#ref. TODO
+#ref. https://medium.com/@petehouston/install-and-config-redis-on-mac-os-x-via-homebrew-eb8df9a4f298
 brew search redis #there should be `redis`
 brew install redis
 
