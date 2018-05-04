aab265e4f77c91a20bda8ac79c1a6a3d61a01a1b
namgivu <namgivu@gmail.com>
2018-05-04 16:03:38 +0700

    review install postgres 10

--- util/postgres/install-postgres-10.sh
+++ util/postgres/install-postgres-10.sh
@@ -36,12 +36,47 @@ sudo apt-get update
 #install postgres 10
 sudo apt-get install -y postgresql-10
 
-#from now on, switch to postgres user
-sudo su - postgres
+#aftermath - ensure that the server is started by switching to the postgres user.
+/usr/lib/postgresql/10/bin/pg_ctl -D /var/lib/postgresql/10/main -l logfile start   #if fails, try with restart command below
+/usr/lib/postgresql/10/bin/pg_ctl -D /var/lib/postgresql/10/main -l logfile restart #TODO can we just call `sudo service postgresql restart`
 
-  #aftermath - ensure that the server is started by switching to the postgres user.
-  /usr/lib/postgresql/10/bin/pg_ctl -D /var/lib/postgresql/10/main -l logfile start   #if fails, try with restart command below
-  /usr/lib/postgresql/10/bin/pg_ctl -D /var/lib/postgresql/10/main -l logfile restart
+#confirm version
+psql -c 'SELECT version();'
 
-  #confirm version
-  psql -c 'SELECT version();'
+
+
+## RUN MANUALLY ##
+
+#config to login with username+password ref. https://stackoverflow.com/a/26735105/248616
+    #get pg_hba.conf path
+    psql -c 'show hba_file' #get path as /etc/postgresql/10/main/pg_hba.conf
+
+    #update postgres's method from `peer` to `trust` at below line
+    sudo nano /etc/postgresql/10/main/pg_hba.conf
+    line="
+# Database administrative login by Unix domain socket
+local   all             postgres                                trust
+"
+    #reload postgres to apply the new config
+    sudo service postgresql restart
+
+    #set postgres password via psql
+    psql -c "ALTER USER postgres with password 'postgres';"
+
+    #more info
+        #trust - anyone who can connect to the server is authorized to access the database
+        #peer  - use client's operating system user name as database user name to access it
+        #md5   - encrypted-password-base authentication
+        #full detail ref. https://www.postgresql.org/docs/9.3/static/auth-methods.html
+
+    #ensure we setup psql connection password ref. https://stackoverflow.com/a/6405162/248616
+    POSTGRES_PORT='5432'         #same as defined in docker-compose.yml
+    POSTGRES_USER='postgres'     #same as defined in docker-compose.yml
+    POSTGRES_PASSWORD='postgres' #same as defined in docker-compose.yml
+    echo "localhost:$POSTGRES_PORT:*:$POSTGRES_USER:$POSTGRES_PASSWORD" >> "$HOME/.pgpass" #TODO script to remove duplicated+continuous lines
+
+    #(optional) create role+user same as user on local machine
+    username="$USER"
+    psql -U postgres -c "CREATE ROLE $username SUPERUSER LOGIN REPLICATION CREATEDB CREATEROLE;"
+    psql -U postgres -c "ALTER USER $username with password 'namgivu';" #set password in case we need to connect via md5/user+pass
+    psql -U postgres -c "CREATE DATABASE namgivu OWNER $username;"
