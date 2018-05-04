e8c901ab33e857334c27a0201a69a03db1d6f0d3
namgivu <namgivu@gmail.com>
2018-05-04 16:15:23 +0700

    review postgis

--- util/postgres/install-postGIS.sh
+++ util/postgres/install-postGIS.sh
@@ -2,7 +2,9 @@
 
 #install PostgreGIS on Ubuntu 16
 #ref. https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-postgis-on-ubuntu-14-04
+#ref. http://www.gis-blog.com/how-to-install-postgis-2-3-on-ubuntu-16-04-lts/
 
 sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable \
-  && sudo apt update && sudo apt install -y postgis
-
+  && sudo apt update \
+  && sudo apt install -y postgis \
+  && sudo apt install -y postgresql-10-postgis-2.4
