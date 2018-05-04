acb78150c2346d1100a3104133261dcbc01cd79a
namgivu <namgivu@gmail.com>
2018-05-05 01:39:01 +0700

    review docker*

--- util/bash_util/docker_util/install-docker-compose.sh
+++ util/bash_util/docker_util/install-docker-compose.sh
@@ -2,7 +2,6 @@
 
 : #install docker-compose command ref. https://stackoverflow.com/a/36689427/248616 -> original guide https://docs.docker.com/compose/install/
 
-"""get latest number at https://github.com/docker/compose/releases/latest"""
 #retrieve latest version 1) grep with regex showing matched part only ref. https://stackoverflow.com/a/3423809/248616, 2) curl without progress ref. https://stackoverflow.com/a/7373922/248616
 latestUrl=`curl -s https://github.com/docker/compose/releases/latest | grep -Eo "(http[^\"]+)"` #sample result of this command https://github.com/docker/compose/releases/tag/1.17.1
 version=`echo "$latestUrl" | cut -d '/' -f8` #bash split string and get nth element ref. https://unix.stackexchange.com/a/312281/17671
--- util/bash_util/docker_util/install-docker.sh
+++ util/bash_util/docker_util/install-docker.sh
@@ -3,13 +3,12 @@
 #applied for Ubuntu 16 only
 
 #remove previous version if any
-sudo apt-get remove docker docker-engine
-
+sudo apt remove docker docker-engine docker.io
 
 ##region set up apt repository
   #ref. https://docs.docker.com/engine/installation/linux/ubuntu/#set-up-the-repository
 
-  sudo apt-get install -y \
+  sudo apt install -y \
       apt-transport-https \
       ca-certificates \
       curl \
@@ -21,12 +20,13 @@ sudo apt-get remove docker docker-engine
      "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
      $(lsb_release -cs) \
      stable"
+
 ##endregion set up apt repository
 
 
 #install docker (CE version)
-sudo apt-get update
-sudo apt-get install -y docker-ce
+sudo apt update
+sudo apt install -y docker-ce
 
 
 #aftermath check
