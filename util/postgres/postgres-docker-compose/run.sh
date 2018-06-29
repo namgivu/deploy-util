4eb5963d4bf989107114e44c9d1e243cb061c445
namgivu <namgivu@gmail.com>
2018-06-29 14:01:18 +0700

    review running postgres as docker-compose

--- util/postgres/postgres-docker-compose/run.sh
+++ util/postgres/postgres-docker-compose/run.sh
@@ -14,6 +14,10 @@ docker-compose -f "$SCRIPT_HOME/docker-compose.yml" -p "$CONTAINER_PREFIX" up --
 CONTAINER_NAME="${CONTAINER_PREFIX}_postgres_1"
 POSTGRES_USER='postgres'
 note="
+aftermath check
+docker ps #must see correct port running e.g. 54322
+psql -h localhost -p 54322 -U postgres #must be able to get in psql with psql prompt
+
 after container run, we can use 'psql' via
 local-machine $ docker exec -it $CONTAINER_NAME bash #ref. https://askubuntu.com/a/507009/22308
 in-container  # psql -U $POSTGRES_USER
